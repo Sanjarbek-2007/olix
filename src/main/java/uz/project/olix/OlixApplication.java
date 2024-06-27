d683485c6b449c930bba00e605b5079cc14ba3f9
asus <yusanjiy@gmail.com>
2024-06-27 12:26:36 +0500

    Olix first

---
+++ src/main/java/uz/project/olix/OlixApplication.java
@@ -0,0 +1,13 @@
+package uz.project.olix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OlixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/project/olix/OlixApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.project.olix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
